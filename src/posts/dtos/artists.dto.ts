d5a3cc6d36b2219a0ab45d8c5bb039d29113376c
Jetty2020 <kmclo@naver.com>
2021-09-24 20:38:57 +0900

    [feat] Delete useless dto

--- src/posts/dtos/artists.dto.ts
+++ src/posts/dtos/artists.dto.ts
@@ -1,25 +1,9 @@
-import { Field, InputType, ObjectType } from '@nestjs/graphql';
-import {
-  PaginationInput,
-  PaginationOutput,
-} from 'src/common/dtos/pagination.dto';
+import { Field, ObjectType } from '@nestjs/graphql';
+import { PaginationOutput } from 'src/common/dtos/pagination.dto';
 import { Artist } from '../entities/artist.entity';
-import { Post } from '../entities/post.entity';
-
-@InputType()
-export class ArtistsInput extends PaginationInput {
-  @Field(() => String, { nullable: true })
-  slug?: string;
-}
 
 @ObjectType()
 export class ArtistsOutput extends PaginationOutput {
-  @Field(() => [Post], { nullable: true })
-  posts?: Post[];
-
   @Field(() => [Artist], { nullable: true })
   artists?: Artist[];
-
-  @Field(() => Artist, { nullable: true })
-  artist?: Artist;
 }
