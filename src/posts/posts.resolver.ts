29b4a0a701be293e0fcca0fa575fcf48d712c3b4
Jetty2020 <kmclo@naver.com>
2021-09-29 15:19:06 +0900

    [feat] Delete role decoration on searchByUser query

--- src/posts/posts.resolver.ts
+++ src/posts/posts.resolver.ts
@@ -60,7 +60,6 @@ export class PostResolver {
   }
 
   @Query(() => SearchByUserOutput)
-  @Role(['Any'])
   searchByUser(
     @Args('input') searchByUserInput: SearchByUserInput,
   ): Promise<SearchByUserOutput> {
