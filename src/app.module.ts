5b48ce865c3869ea444df2375dab503a10a29a64
Jetty2020 <kmclo@naver.com>
2021-10-01 19:53:11 +0900

    [fix] Fix typeOrmMudule

--- src/app.module.ts
+++ src/app.module.ts
@@ -44,8 +44,8 @@ import { UploadsModule } from './upload/uploads.module';
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
           }),
-      synchronize: process.env.NODE_ENV !== 'production',
-      logging: process.env.NODE_ENV !== 'production',
+      synchronize: process.env.NODE_ENV !== 'prod',
+      logging: process.env.NODE_ENV !== 'prod',
       entities: [User, Post, Artist, Like, Comment],
     }),
     GraphQLModule.forRoot({
